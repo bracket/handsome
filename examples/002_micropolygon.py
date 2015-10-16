Precision bug fixes in biliniear inverter and add reference NumPy implementation

examples/002_micropolygon.py
@@ -14,34 +14,29 @@ def main():
     }
 
     for key, value in colors.items():
-        # value = tuple(c / 255. for  c in parse_color(value))
-        # colors[key] = np.array([ value ], dtype=FloatPixel)
         colors[key] = np.array([ parse_color(value) ], dtype=Pixel)
 
     image = Tile((0, 0), (512, 512), dtype=Pixel)
     image.buffer[:,:]    = colors['white']
-    # image.buffer[128:,:] = colors['red']
 
     m = Micropolygon(
-        point(0,   0),
-        point(0, 256),
-        point(512, 256),
-        point(512, 512),
+        point(16,   16),
+        point(128, 16),
+        point(512 - 128, 512 - 16),
+        point(512 - 16, 512 - 16),
     )
 
-    # abscissae = [ (0, 0), (1, 0), (1, 1), (0, 1), ]
-    # ordinates = [ m(*p) for p in abscissae ]
+    buffer = image.buffer
+    coordinate_image = image.coordinate_image
 
-    # lines =  [ Line(start, end) for start, end in zip(ordinates, ordinates[1:]) ]
-    # lines.append(Line(ordinates[-1], ordinates[0]))
-
-    # for line in lines:
-    #     print(line)
-
-    # for p in image.coordinate_image:
-    #     print(p)
+    blue = parse_color('#00f')
 
     width, height = image.shape
+    for i in range(width):
+        for j in range(height):
+            coordinate = coordinate_image[i, j]
+            if m.inverse_solve(coordinate['x'], coordinate['y']):
+                buffer[i,j] = blue
 
     fill(
         generate_numpy_begin(m.points[0][0]),
@@ -49,7 +44,7 @@ def main():
         generate_numpy_begin(m.points[1][0]),
         generate_numpy_begin(m.points[1][1]),
         width, height,
-        generate_numpy_begin(image.coordinate_image),
+        generate_numpy_begin(coordinate_image),
         generate_numpy_begin(image.buffer)
     )
 

handsome/Tile.py
@@ -1,46 +1,9 @@
 __all__ =  [ 'Tile' ]
 
+from .Coordinate import Coordinate
 from .Interval import Interval
 from .Pixel import Pixel
 import numpy as np
-
-class Interval(object):
-    '''Interval - Arbitrary half-closed interval of the form [start, end)'''
-
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-
-    def __call__(self, value):
-        return (1. - value) * self.start + value * self.end
-
-    def __str__(self):
-        return 'Interval({self.start}, {self.end})'.format(self=self)
-
-    def __repr__(self):
-        return 'Interval({self.start}, {self.end})'.format(self=self)
-
-    def __eq__(self, right):
-        return self.start == right.start and self.end == right.end
-
-    def contains(self, value):
-        return self.start <= value < self.end
-
-    def __intersection(self, right):
-        return (
-            max(self.start, right.start),
-            min(self.end, right.end)
-        )
-
-    def overlaps(self, other):
-        start, end = self.__intersection(other)
-        return start < end
-
-    def intersection(self, other):
-        start, end = self.__intersection(other)
-
-        if start < end: return Interval(start, end)
-        else: return None
 
 
 class Tile:
@@ -58,6 +21,7 @@ class Tile:
         self.origin     = origin
         self.horizontal = Interval(origin[0], origin[0] + self.shape[0])
         self.vertical   = Interval(origin[1], origin[1] + self.shape[1])
+        self.__coordinate_image = None
 
 
     def contains_point(self, point):
@@ -124,9 +88,12 @@ def make_coordinate_image(origin, shape):
     xs = np.arange(origin[0], origin[0] + shape[0], dtype=np.float32)
     ys = np.arange(origin[1] + shape[1] - 1, origin[1] - 1, -1, dtype=np.float32)
 
-    out = np.zeros(list(shape) + [ 2 ], dtype=np.float32)
+    out = np.zeros(list(shape), dtype=Coordinate, order='F')
 
-    for x in range(shape[1]): out[:,x,0] = xs 
-    for y in range(shape[0]): out[y,:,1] = ys
+    for x in range(shape[0]):
+        out[x,:]['y'] = ys
+
+    for y in range(shape[1]):
+        out[:,y]['x'] = xs
 
     return out

tests/test_tile.py
@@ -1,5 +1,8 @@
-from handsome.Tile import Tile
+from handsome.Coordinate import Coordinate
 from handsome.Interval import Interval
+from handsome.Tile import Tile
+
+import numpy as np
 
 def test_tile():
     tile = Tile((0, 0), (100, 200))
@@ -15,3 +18,15 @@ def test_tile():
     assert tile.shape == (100, 200)
     assert tile.horizontal == Interval(25, 125)
     assert tile.vertical == Interval(50, 250)
+
+def test_coordinate_image():
+    tile = Tile((1, 2), (2, 3))
+    actual = tile.coordinate_image
+
+    expected = np.array([
+        [ (1, 4), (2, 4) ],
+        [ (1, 3), (2, 3) ],
+        [ (1, 2), (2, 2) ],
+    ], dtype=Coordinate).T
+
+    np.testing.assert_array_equal(expected, actual)