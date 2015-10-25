6001e1df5eee68e5411218993017edd1f7ea3874
Stephen McCray <smc@rentrak.com>
2015-10-24 23:03:05 -0700

    Fix Micropolygon test

--- tests/test_micropolygon.py
+++ tests/test_micropolygon.py
@@ -11,14 +11,14 @@ def test_micropolygon():
     )
 
     tests = [
-        [ (.5, .5),   np.array([ 1, .5, 1  ]), ],
-        [ (.25, .75), np.array([ 1, .75, 1 ]), ],
-        [ (0., 0.),   np.array([ 0,   0, 1 ]), ],
-        [ (1., 0.),   np.array([ 1,   0, 1 ]), ],
-        [ (0., 1.),   np.array([ 1,   1, 1 ]), ],
-        [ (1., 1.),   np.array([ 2,   1, 1 ]), ],
+        [ (.5 , .5) , np.array([ 1, .5 , 1, 1 ]), ],
+        [ (.25, .75), np.array([ 1, .75, 1, 1 ]), ],
+        [ (0. , 0.) , np.array([ 0, 0  , 1, 1 ]), ],
+        [ (1. , 0.) , np.array([ 1, 0  , 1, 1 ]), ],
+        [ (0. , 1.) , np.array([ 1, 1  , 1, 1 ]), ],
+        [ (1. , 1.) , np.array([ 2, 1  , 1, 1 ]), ],
     ]
 
     for input, expected in tests:
         actual = m(*input)
-        assert (expected == actual).all()
+        np.testing.assert_array_equal(actual, expected)
